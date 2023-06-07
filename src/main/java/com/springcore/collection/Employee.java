34b3afa6c1b0bad3dc947f15c929ea85b02f683b
krunal1 <krunal1@gmail.com>
2023-06-07 07:30:07 +0530

    second commit

---
+++ src/main/java/com/springcore/collection/Employee.java
@@ -0,0 +1,55 @@
+package com.springcore.collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Employee {
+    private String Name;
+    private List<String>Phone;
+    private Set<String>Addresses;
+    private Map<String ,Integer>courses;
+
+    public Employee(){
+        super();
+    }
+
+    public Employee(String name, List<String> phone, Set<String> addresses, Map<String, Integer> courses) {
+        Name = name;
+        Phone = phone;
+        Addresses = addresses;
+        this.courses = courses;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public List<String> getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(List<String> phone) {
+        Phone = phone;
+    }
+
+    public Set<String> getAddresses() {
+        return Addresses;
+    }
+
+    public void setAddresses(Set<String> addresses) {
+        Addresses = addresses;
+    }
+
+    public Map<String, Integer> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Map<String, Integer> courses) {
+        this.courses = courses;
+    }
+}
